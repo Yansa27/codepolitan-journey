0cb8c83cafca151ce0469ae5428a1622da9a7f57
Yansa27 <juliansa837@gmail.com>
2023-03-01 08:56:36 +0700

    push

---
+++ day3.js
@@ -0,0 +1,34 @@
+// Loop
+
+// 1 For loop 
+
+for (let i = 0 ; i <= 10 ; i++) {
+    console.log(`angka ke ${i}`);
+}
+
+// 2 for of
+let name = ["yansa" , "lina" , "bangkit"];
+
+for (let new_Name of name) {
+    console.log(new_Name);
+}
+
+//  while
+
+let i = 1;
+
+while ( i <= 10) {
+    let a = 1;
+        if (a < 10) {
+            a++
+            console.log(a);
+           
+        }else{
+            console.log("terpernuhi");
+        }
+    i++    
+}
+
+
+
+
